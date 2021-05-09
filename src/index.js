7718cd345ef9b32f70ce8f5014e48e80164de567
Nikita Deputatov <deputatovn@gmail.com>
2021-05-09 21:41:54 +0300

    Test husky

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-const username = 'freddy';
-
-if (!('serviceWorker' in navigator)) {
-  // you have an old browser :-(
-}
+// if (!('serviceWorker' in navigator)) {
+// you have an old browser :-(
+// }
 
 const greeting = 'hello';
+
 console.log(`${greeting} world!`);
 [(1, 2, 3)].forEach((x) => console.log(x));
 
